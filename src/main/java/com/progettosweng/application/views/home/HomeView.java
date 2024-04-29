13917dd2d4456fb5f262fec634cc8d6aa79d314d
bonfito <bonifazijacopo@gmail.com>
2024-04-29 17:45:38 +0200

    Aggiunta descrizione per il  funzionamento dell'applicazione

--- src/main/java/com/progettosweng/application/views/home/HomeView.java
+++ src/main/java/com/progettosweng/application/views/home/HomeView.java
@@ -1,6 +1,7 @@
 package com.progettosweng.application.views.home;
 
 import com.progettosweng.application.views.MainLayout;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -32,6 +33,18 @@ public class HomeView extends VerticalLayout {
             add(new H1("Benvenuto "+username));
         }
 
-        add(new H2("Le tue Storie"));
+
+        Div tab = new Div();
+        tab.getStyle().set("border-radius", "10px");
+        tab.getStyle().set("border", "1px solid #ccc");
+        tab.setWidthFull();
+        tab.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed magna turpis, vehicula ac nulla nec, posuere rutrum odio. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris nec egestas lorem. Sed tincidunt tempus massa. In eu ligula lorem. Sed nec leo nunc. Cras ex diam, mollis eget urna et, egestas tincidunt eros.\n" +
+                "\n" +
+                "Nullam a varius turpis. Donec sit amet scelerisque mauris. Nam eu posuere purus. Vestibulum sit amet elit odio. Nullam pretium, nunc at imperdiet iaculis, leo arcu efficitur felis, id lacinia purus eros in risus. Praesent dictum ut tortor a cursus. Duis vel erat ante. Cras semper, mauris nec posuere vestibulum, nunc orci consectetur nunc, fringilla eleifend neque nisl tincidunt nisi. Nulla quis rhoncus ex, a venenatis mi.");
+        tab.setHeight("200px");
+        add(tab);
+
+        add(new H2("Le tue Storie: "));
+
     }
 }
