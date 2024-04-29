dd9c62faf604a9d907aabc14897c912a8d0c0f97
Leonardo Vorabbi <vorabbi.leonardo@gmail.com>
2024-04-29 18:17:43 +0200

    Task 5 per UC2 - Implementazione bottoni per documenti

--- src/main/java/com/progettosweng/application/views/home/HomeView.java
+++ src/main/java/com/progettosweng/application/views/home/HomeView.java
@@ -1,8 +1,9 @@
 package com.progettosweng.application.views.home;
 
 import com.progettosweng.application.views.MainLayout;
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -12,6 +13,7 @@ import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import java.util.Objects;
 
 @PageTitle("Home")
 @Route(value = "home", layout = MainLayout.class)
@@ -22,16 +24,55 @@ import org.springframework.security.core.context.SecurityContextHolder;
 public class HomeView extends VerticalLayout {
 
     public HomeView() {
+        setAlignItems(Alignment.CENTER);
         // Ottieni l'oggetto di autenticazione corrente
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String username = authentication.getName();
         // Controlla se l'utente è autenticato
-        if (username == "anonymousUser") {
+        if (Objects.equals(username, "anonymousUser")) {
             add(new H1("Benvenuto guest"));
         } else {
             add(new H1("Benvenuto "+username));
         }
 
-        add(new H2("Le tue Storie"));
+        HorizontalLayout horizontalLayout = new HorizontalLayout();
+
+        // Creazione di un bottone che apre il PDF in una nuova scheda del browser
+        Button buttonManualeUtente = new Button("");
+        Anchor anchorUtente = new Anchor("https://docs.google.com/document/d/1DcI4OWJ2dbmKapmuQSLBzTiJIVoyyo8rAk8qQzBM-bM/edit?usp=sharing", "Vai al documento");
+        anchorUtente.setTarget("_blank");
+        buttonManualeUtente.getElement().appendChild(anchorUtente.getElement());
+
+        // Creazione di un bottone che apre il PDF in una nuova scheda del browser
+        Button buttonManualeSviluppatore = new Button("");
+        Anchor anchorSviluppatore = new Anchor("https://docs.google.com/document/d/1DkqmRd9IfRRmYg-jhj_Q5sjLzQEj_GZI2FO8WXrRJLs/edit?usp=sharing", "Vai al documento");
+        anchorSviluppatore.setTarget("_blank");
+        buttonManualeSviluppatore.getElement().appendChild(anchorSviluppatore.getElement());
+
+        VerticalLayout layoutDiv1 =  new VerticalLayout(new H3("Vai al manuale dell'utente"), buttonManualeUtente);
+        layoutDiv1.setAlignItems(Alignment.CENTER);
+
+        VerticalLayout layoutDiv2 =  new VerticalLayout(new H3("Vai al manuale dello sviluppatore"), buttonManualeSviluppatore);
+        layoutDiv2.setAlignItems(Alignment.CENTER);
+
+        Div container1 = new Div();
+        container1.getStyle().setBackground("#154c79");
+        container1.getStyle().set("padding", "20px");
+        container1.getStyle().set("border-radius", "10px");
+        container1.add(layoutDiv1);
+
+
+        Div container2 = new Div();
+        container2.getStyle().setBackground("#154c79");
+        container2.getStyle().set("padding", "20px");
+        container2.getStyle().set("border-radius", "10px");
+        container2.add(layoutDiv2);
+
+        horizontalLayout.add(container1, container2);
+
+        add(horizontalLayout);
     }
+
+
+
 }
